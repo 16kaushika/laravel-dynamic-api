a95878308ca519c2b5de0b47b7f42025dcbc45f2
Gohil Jaykumar <gohiljaykumar1996@gmail.com>
2024-10-13 04:15:54 +0530

    1

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\VerifyCsrfToken;
-use App\Http\Middleware\CustomCors;
+// use App\Http\Middleware\CustomCors;
 
 return Application::configure(basePath: dirname(__DIR__))
 ->withRouting(
@@ -18,7 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
     );
     
     // Add the custom CORS middleware
-    $middleware->add(CustomCors::class);
+    // $middleware->add(CustomCors::class);
 })
 ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DataHubController;
 // use Fruitcake\Cors\HandleCors;
+use App\Http\Middleware\CustomCors;
 
 Route::get('/', function () { return view('getting-started'); })->name('getting-started');
 Route::get('/quick-start', function () { return view('quick-start'); })->name('quick-start');
 Route::get('/demo', function () { return view('demo'); })->name('demo');
 
 // Defining API routes within web.php
-Route::prefix('api')->middleware(['api'])->group(function () {
+Route::prefix('api')->middleware([CustomCors::class,'api'])->group(function () {
     Route::get('{project}/{module}', [DataHubController::class, 'getDataList']);
     Route::post('{project}/{module}', [DataHubController::class, 'create']);
     Route::get('{project}/{module}/{dataId}', [DataHubController::class, 'getDetail']);
